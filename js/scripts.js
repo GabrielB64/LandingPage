b6863fe6050840b515fb66edefcac6126fcd4d0d
Gabriel Bustamante <gabriel.bustamante04@inacapmail.cl>
2024-04-24 21:24:49 -0400

    Primer deploy

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+function mostrarSuccess(){
+    var email = document.getElementById("correo").value;
+    document.getElementById("textoSuscrito").innerHTML = "Te has suscrito con el correo " + email;
+}
